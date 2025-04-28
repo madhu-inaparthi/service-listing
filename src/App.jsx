f2cf01e95a07e9716d3d083bfd1080454a40869f
madhu-software <inaparthi.madhukiran.s89@kalvium.community>
2025-04-28 14:32:32 +0530

    completed the assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ServiceCard from './components/ServiceCard'
 import data from "../data.json"
